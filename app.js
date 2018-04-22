c84c725d1c6d6db3db12d064927fb3d8135381c4
Aarushi <aarushisharda@gmail.com>
2018-04-22 19:52:51 +0530

    cat duo

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let count = 0;
+let counts = 0;
+
+document.getElementById("nameone").innerHTML = "Oranges";
+document.getElementById("nametwo").innerHTML = "Apples";
+
 var elem = document.getElementById('cat-img');
 elem.addEventListener('click', function(){
 	count = count+1;
 	document.getElementById("counter").innerHTML = count;
+}, false);
+
+var element = document.getElementById('cat-img-2');
+element.addEventListener('click', function(){
+	counts = counts + 1;
+	document.getElementById("counters").innerHTML = counts;
 }, false);
